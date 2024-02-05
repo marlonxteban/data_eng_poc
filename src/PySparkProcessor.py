951ca887b469935fa843b43a69c2ad04d551b7cd
marlonxteban <marlonxteban@gmail.com>
2024-02-04 22:12:14 -0500

    feat: Update employees endpoint to allow batch processing.

--- src/PySparkProcessor.py
+++ src/PySparkProcessor.py
@@ -1,14 +1,73 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+
+from datetime import datetime
 
 class PySparkProcessor:
     def __init__(self):
+        self.url = "jdbc:postgresql://host.docker.internal:5432/postgres"
+        self.properties = {
+            "user": "postgres",
+            "password": "postgres",
+            "driver": "org.postgresql.Driver"
+        }
         self.spark = SparkSession.builder \
             .appName("data_eng_poc") \
+            .config("spark.jars", "./drivers/postgresql-42.7.1.jar") \
             .getOrCreate()
 
-    def process_csv(self, file_path):
+    def get_schema(self, name):
+        schemas = {
+            "job": StructType([
+                StructField("id", IntegerType(), False),
+                StructField("job", StringType(), False)
+            ]),
+            "department": StructType([
+                StructField("id", IntegerType(), False),
+                StructField("department", StringType(), False)
+            ]),
+            "employee": StructType([
+                StructField("id", IntegerType(), False),
+                StructField("name", StringType(), False),
+                StructField("datetime", DateType(), False),
+                StructField("department_id", IntegerType(), True),
+                StructField("job_id", IntegerType(), True)
+            ])
+        }
+        return schemas[name]
+
+    def get_table_name(self, name):
+        table_names = {
+            "job": "jobs",
+            "department": "departments",
+            "employee": "employees"
+        }
+        return table_names[name]
+
+    def process_csv(self, file_path, file_type):
         # read file
-        df = self.spark.read.csv(file_path, header=False, inferSchema=True)
-        # TODO: replace with actual processing
-        df.show()
-        df.printSchema()
+        schema = self.get_schema(file_type)
+        df = self.spark.read.csv(file_path, schema=schema, header=False)
+        df.write.jdbc(url=self.url, table=self.get_table_name(file_type), mode="append", properties=self.properties)
+        return df.count()
+
+    def preprocess_employees_file(self, file_path, blocks):
+        # read file
+        schema = self.get_schema("employee")
+        df = self.spark.read.csv(file_path, schema=schema, header=False)
+        # preprocess
+        invalid_records_df = df.filter(df["id"].isNull() | df["name"].isNull() | df["datetime"].isNull())
+        invalid_records_df.write.csv(f"rejected_files/invalid_records_{datetime.now()}", header=True, mode="overwrite")
+        valid_records_df = df.filter(df["id"].isNotNull() & df["name"].isNotNull() & df["datetime"].isNotNull())
+        valid_date_records_df = valid_records_df.withColumn("datetime", df["datetime"].cast("date"))
+        return valid_date_records_df
+
+    def calculate_partitions(self, count, blocks):
+        return count // blocks if count % blocks == 0 else (count // blocks) + 1
+
+    def process_employees_df(self, df, blocks):
+        valid_employees = df.count()
+        partitions = self.calculate_partitions(valid_employees, blocks)
+        df = df.repartition(partitions)
+        df.write.jdbc(url=self.url, table=self.get_table_name("employee"), mode="append", properties=self.properties)
+        return valid_employees
--- src/main.py
+++ src/main.py
@@ -7,50 +7,51 @@ from PySparkProcessor import PySparkProcessor
 
 app = FastAPI()
 
-os.makedirs('batch_files', exist_ok=True)
-os.makedirs('processed_batches', exist_ok=True)
+os.makedirs('rejected_files', exist_ok=True)
 os.makedirs('in_process', exist_ok=True)
 os.makedirs('processed_files', exist_ok=True)
 
-@app.post("/process")
-async def process_file(file: UploadFile = File(...)):
-    # Process File
+@app.post("/load_jobs")
+async def process_jobs_file(file: UploadFile = File(...), bloks: int = 1):
+    file_path, filename = save_file(file)
+    return process_file("job", file_path, filename, bloks)
+
+@app.post("/load_departments")
+async def process_departments_file(file: UploadFile = File(...), bloks: int = 1):
+    file_path, filename = save_file(file)
+    return process_file("department", file_path, filename, bloks)
+
+@app.post("/load_employees")
+async def process_departments_file(file: UploadFile = File(...), bloks: int = 1):
+    file_path, filename = save_file(file)
+    return process_file("employee", file_path, filename, bloks)
+
+def save_file(file: UploadFile) -> (str, str):
     temp_file_path = f"./in_process/{file.filename}"
     with open(temp_file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+    return temp_file_path, file.filename
+
+def process_file(file_type: str, file_path: str, filename: str, blocks: int):
     # Process here
     try:
         processor = PySparkProcessor()
-        processor.process_csv(temp_file_path)
-        message = "File processed successfully"
+        needs_preprocess = file_type == "employee"
+        processed_records = 0
+        if needs_preprocess:
+            valid_employees_df = processor.preprocess_employees_file(file_path, blocks)
+            processed_records = processor.process_employees_df(valid_employees_df, blocks)
+        else:
+            processed_records = processor.process_csv(file_path, file_type)
+
+        message = f"File processed successfully, records inserted: {processed_records}"
+        # move files after processing
+        dest_file_path = f'processed_files/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{filename}'
+        shutil.move(file_path, dest_file_path)
     except Exception as e:
         message = f"Error processing file: {e}"
-
-    # move files after processing
-    dest_file_path = f'processed_files/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{file.filename}'
-    shutil.move(temp_file_path, dest_file_path)
+        # move to rejected folder
+        dest_file_path = f'rejected_files/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{filename}'
+        shutil.move(file_path, dest_file_path)
 
     return {"message": f"{message}"}
-
-@app.post("/batch")
-async def upload_batch_file(file: UploadFile = File(...)):
-    # Save File in batch_files folder
-    file_location = f"batch_files/{file.filename}"
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {"message": f"File saved for batch processing: {file.filename}"}
-
-@app.post("/process_batch")
-async def process_batch_files():
-    batch_files = os.listdir('batch_files')
-    if not batch_files:
-        return {"message": "No batch files to process"}
-
-    for filename in batch_files:
-        # Process here
-        src_file_path = f'batch_files/{filename}'
-        dest_file_path = f'processed_batches/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{filename}'
-        # move files after processing
-        shutil.move(src_file_path, dest_file_path)
-
-    return {"message": "All batch files processed successfully"}
